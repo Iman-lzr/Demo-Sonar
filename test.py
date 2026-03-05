84dccb6aa10c6f9dbb5783b117e6e6ce95a2014e
Iman Lzr <imanelaazraoui@gmail.com>
2026-03-05 10:52:02 +0000

    change

--- test.py
+++ test.py
@@ -1,13 +1,4 @@
-def greet_user(name):
-    message = f"Bonjour {name}"
-    print(message)
+def afficher_nom(nom):
+    print("Votre nom est :", nom)
 
-def calculate_sum(a, b):
-    result = a + b
-    return result
-
-
-greet_user("Iman")
-
-total = calculate_sum(5, 3)
-print("La somme est :", total)
+afficher_nom("Iman")
